f2895ed6e2d5f9b68601995e758ae2b39521a121
flemay <flemay@gmail.com>
2024-02-14 23:18:08 +0100

    [1c] Composante nomination aux Oscars fonctionnelle

--- parts/oscars-nomination/oscars-nomination-section.php
+++ parts/oscars-nomination/oscars-nomination-section.php
@@ -1,5 +1,17 @@
-<!-- Il semblerait qu'on ne puisse pas appeler la feuille de style ici. -->
-<section id="oscars-nomination">
-  <h2>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h2>
-  <img src="<?php echo get_stylesheet_directory_uri() . '/parts/oscars-nomination/images/oscars_logo.png'; ?>" alt="">
-</section>
+<?php
+  // Style pour la section temporaire de la mise en nomination aux Oscars
+  // Dans le foote car on ne veut pas à devoir l'appeler ailleurs qu'ici (module indépendant).
+  // Ça ne serait pas une bonne pratique à faire. 
+  add_action( 'wp_footer', 'callback_oscar_nomination_style');
+  function callback_oscar_nomination_style () {
+    wp_enqueue_style('oscars-nomination-style', 
+      get_stylesheet_directory_uri() . '/parts/oscars-nomination/style.css' );
+  }
+?>
+
+<div id="oscars-nomination">
+  <!-- <div> -->
+    <h2>Fleurs d’oranger & chats errants<br>est nominé aux Oscars Short<br>Film Animated de 2022 !</h2>
+    <img src="<?php echo get_stylesheet_directory_uri() . '/parts/oscars-nomination/images/oscars_logo.png'; ?>" alt="">
+  <!-- </div> -->
+</div>
