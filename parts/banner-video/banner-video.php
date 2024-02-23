26b7b07070b17059e01e613735cd0edddd0a5a9a
flemay <flemay@gmail.com>
2024-02-23 12:59:18 +0100

    [2a] Créer une version plus légère de l'image banner, qui sert au fallback de la vidéo

--- parts/banner-video/banner-video.php
+++ parts/banner-video/banner-video.php
@@ -1,5 +1,5 @@
 <?php
-  // Style pour la section temporaire de la mise en nomination aux Oscars
+  // Style pour la vidéo sur la bannière. 
   // Dans le foote car on ne veut pas à devoir l'appeler ailleurs qu'ici (module indépendant).
   // Ça ne serait pas une bonne pratique à faire. 
   add_action( 'wp_footer', 'callback_banner_video_style');
@@ -9,6 +9,6 @@
   }
 ?>
 
-<video id="banner-video" autoplay loop muted poster= "<?php echo get_template_directory_uri() . '/assets/images/banner.png'; ?>">
+<video id="banner-video" autoplay loop muted poster= "<?php echo get_stylesheet_directory_uri() . '/assets/images/banner_1440x806-fs8.png'; ?>">
   <source src="<?php echo get_stylesheet_directory_uri() . '/parts/banner-video/video/Studio_Koukaki_video_header.mp4'; ?>" type="video/mp4">
 </video>
