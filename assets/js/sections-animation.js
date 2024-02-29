50896a693faab255d602a55e497833c6799a13bd
flemay <flemay@gmail.com>
2024-02-29 10:30:23 +0100

    [1a] Ajouter animation translateY des sections. TODO : animation de la bannière

--- assets/js/sections-animation.js
+++ assets/js/sections-animation.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   let observerOptions = {
     rootMargin: "0px",
     threshold: 0.1,
   };
 
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add('is-visible');
+        entry.target.classList.add("is-visible", "slideUp");
+        const allParagraphs = entry.target.querySelectorAll("p");
+        for (const paragraph of allParagraphs)
+          paragraph.classList.add("slideDown");
       }
     });
-  }, observerOptions ); 
+  }, observerOptions);
 
-  const allSections = document.querySelectorAll('section');
+  // const allSections = document.querySelectorAll("section:not(.banner)");
+  const allSections = document.querySelectorAll("section");
   allSections.forEach((section) => {
-    observer.observe(section); // observe each element 
-  })
-
+    observer.observe(section); // observe each element
+  });
 });
