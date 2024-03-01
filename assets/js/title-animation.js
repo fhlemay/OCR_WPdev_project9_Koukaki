03cb86fdef3693b2ddbf00836c93f602ef295fd2
flemay <flemay@gmail.com>
2024-03-01 12:49:36 +0100

    [1a] Ajouter un delais à l'animation des titre pour attendre la fin de l'animation des sections

--- assets/js/title-animation.js
+++ assets/js/title-animation.js
@@ -1,34 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  document.querySelectorAll('h2').forEach(element => {
-
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("h2").forEach((element) => {
     // Splits the text into words and wraps them in a span container.
-    element.innerHTML = 
-      element.textContent
-             .split(" ")
-             .map(word => `<span>${word}</span>`)
-             .join(" ");
+    element.innerHTML = element.textContent
+      .split(" ")
+      .map((word) => `<span>${word}</span>`)
+      .join(" ");
 
-
-    const words = element.querySelectorAll('span');
+    const words = element.querySelectorAll("span");
 
     let observerOptions = {
       rootMargin: "0px",
       threshold: 1.0,
     };
 
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          words.forEach((word, index) => {
-            setTimeout(() => {
-              word.classList.add('is-visible');
-            }, index * 150); // animation delay between words 
-          });
+          setTimeout(() => {
+            words.forEach((word, index) => {
+              setTimeout(() => {
+                word.classList.add("is-visible");
+              }, index * 150); // animation delay between words
+            });
+          }, 2000); // give time to the section's animation
           observer.disconnect(); // stop observing once animation is triggered
         }
       });
-    }, observerOptions ); 
+    }, observerOptions);
 
     observer.observe(element); // observe each element (h2)
   });
