903a1441c710c07ade0507b02d4f9200ad134866
flemay <flemay@gmail.com>
2024-03-02 10:09:11 +0100

    [2c] Ajouter effet de parallaxe entre le logo et la bannière lorsque du scroll vers le bas
    [2c] BUg correction : empêcher le logo d'être sticky lorsque la page est rechargée sans être tout en haut

--- assets/js/banner-animation.js
+++ assets/js/banner-animation.js
@@ -5,8 +5,42 @@ document.addEventListener("DOMContentLoaded", () => {
   // Start the floating animation after the sliding up
   setTimeout(function () {
     logo.classList.add("banner__logo--floating");
+    if (window.scrollY === 0) {
+      // don't get sticky if middle page reload
+      logo.classList.add("banner__logo--sticky");
+    }
   }, 2000);
 
   banner.classList.add("banner--slide-down");
   logo.classList.add("banner__logo--slide-up");
+
+  // When logo leaves viewport, reset position
+  const observer = new IntersectionObserver((changes) => {
+    for (const change of changes) {
+      if (!change.isIntersecting) {
+        logo.style.bottom = "";
+        logoLeftedVP = true;
+      }
+    }
+  });
+  observer.observe(logo);
+});
+
+// Remove logo's stickyness when it touched the bottom borde of the banner
+window.addEventListener("scroll", () => {
+  let logo = document.querySelector(".banner__logo");
+  let banner = document.querySelector(".banner");
+
+  const bannerRect = banner.getBoundingClientRect();
+  const logoRect = logo.getBoundingClientRect();
+
+  if (logoRect.bottom >= bannerRect.bottom) {
+    logo.classList.remove("banner__logo--sticky");
+    logo.style.bottom = "0";
+  }
+
+  // Reset the skickyness if at the top of the page
+  if (window.scrollY === 0) {
+    logo.classList.add("banner__logo--sticky");
+  }
 });
