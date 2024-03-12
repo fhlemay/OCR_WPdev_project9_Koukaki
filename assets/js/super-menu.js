0e90d28c5b5b4638b33c7bca5ec84a99ac38313a
flemay <flemay@gmail.com>
2024-03-12 11:06:51 +0100

    [5] Améliorer la lisibilité du code js du super menu

--- assets/js/super-menu.js
+++ assets/js/super-menu.js
@@ -1,37 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const superMenu = document.querySelector(".super-menu");
-  const toggleButton = document.getElementById("toggle-super-menu");
   const MINI_DELAY = 50; // to avoid the simultaneity of operations
   const ANIM_DELAY = 1000 + MINI_DELAY; // must be higher than in css
-  const openMenuImage =
+
+  // childThemeParams is given by WP when this script is loaded.
+  // See child theme functions.php.
+  const OPEN_MENU_IMAGE =
     childThemeParams.themeUrl + "/assets/images/menu_open.png";
-  const closeMenuImage =
+  const CLOSE_MENU_IMAGE =
     childThemeParams.themeUrl + "/assets/images/menu_close.png";
+
+  const toggleButton = document.getElementById("toggle-super-menu");
+  const superMenu = document.querySelector(".super-menu");
 
   // Initial state : super menu is closed
   superMenu.style.display = "none";
   superMenu.classList.remove("is-visible");
-  toggleButton.src = openMenuImage;
+  toggleButton.src = OPEN_MENU_IMAGE;
 
-  /*
-   * On click change the toggle button image
-   * and show/hide super menu.
-   */
   toggleButton.addEventListener("click", () => {
     if (superMenu.classList.contains("is-visible")) {
-      // close super menu
-      superMenu.classList.remove("is-visible");
-      toggleButton.src = openMenuImage;
-      setTimeout(() => {
-        superMenu.style.display = "none";
-      }, ANIM_DELAY);
+      toggleSuperMenu("CLOSE");
     } else {
-      // open super menu
-      toggleButton.src = closeMenuImage;
-      superMenu.style.display = "flex";
-      setTimeout(() => {
-        superMenu.classList.add("is-visible");
-      }, MINI_DELAY);
+      toggleSuperMenu("OPEN");
     }
   });
+
+  const toggleSuperMenu = (action) => {
+    switch (action) {
+      case "OPEN":
+        superMenu.style.display = "flex";
+        toggleButton.src = CLOSE_MENU_IMAGE;
+        setTimeout(() => {
+          superMenu.classList.add("is-visible");
+        }, MINI_DELAY);
+        break;
+      case "CLOSE":
+        superMenu.classList.remove("is-visible");
+        toggleButton.src = OPEN_MENU_IMAGE;
+        setTimeout(() => {
+          superMenu.style.display = "none";
+        }, ANIM_DELAY);
+        break;
+    }
+  };
 });
