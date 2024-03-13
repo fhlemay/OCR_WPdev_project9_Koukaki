a7f639980da7ba8863477df824f0196e5e75c47c
flemay <flemay@gmail.com>
2024-03-13 10:52:56 +0100

    Refactor banner-animation.js

--- assets/js/banner-animation.js
+++ assets/js/banner-animation.js
@@ -2,27 +2,27 @@ document.addEventListener("DOMContentLoaded", () => {
   const TOP = 0;
   const RESET_POS = ""; // initial position
   const BOTTOM_POS = "0"; // bottom of the container
-  const STICKY_CLASS = "banner__logo--sticky";
-  const FLOATING_CLASS = "banner__logo--floating";
-  const SLIDE_DOWN_CLASS = "banner--slide-down";
-  const SLIDE_UP_CLASS = "banner__logo--slide-up";
+  const STICKY = "banner__logo--sticky";
+  const FLOATING = "banner__logo--floating";
+  const SLIDE_DOWN = "banner--slide-down";
+  const SLIDE_UP = "banner__logo--slide-up";
   const ANIMATION_DELAY = 2000;
-  const vertical_page_position = window.scrollY;
+  const windowPosition = window.scrollY;
   const logo = document.querySelector(".banner__logo");
   const banner = document.querySelector(".banner");
 
-  // Apparition animation
-  banner.classList.add(SLIDE_DOWN_CLASS);
-  logo.classList.add(SLIDE_UP_CLASS);
+  // Initial apparition
+  banner.classList.add(SLIDE_DOWN);
+  logo.classList.add(SLIDE_UP);
 
   // Start the floating animation *after* the sliding to avoid any conflict
   // ... and get sticky.
-  setTimeout(function () {
-    if (vertical_page_position === TOP) {
-      // dont't get sticky if not at the top of the page (reload)
-      logo.classList.add(STICKY_CLASS);
+  setTimeout(() => {
+    if (windowPosition === TOP) {
+      // get sticky only if at the top of the page (reload in middle of the page)
+      logo.classList.add(STICKY);
     }
-    logo.classList.add(FLOATING_CLASS);
+    logo.classList.add(FLOATING);
   }, ANIMATION_DELAY);
 
   /* Dirty trick to make sure that the size and position is not relative to his parent.
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
    * viewport. If width is set to 50%, then the size of the element is adjusted relative
    * to the viewport width. Idem with margin-top.
    */
-  logo.style.width = `${banner.offsetWidth / 2}px`; // 50% of is parent.
+  logo.style.width = `${banner.offsetWidth / 2}px`; // 50% of is parent container.
   logo.style.marginTop = "-100px"; // Rought estim. Couldn't figure that one out.
 
   window.addEventListener("scroll", () => {
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Reset the skickyness if at the top of the page (after scrolling)
     if (window.scrollY === TOP) {
       logo.style.bottom = RESET_POS;
-      logo.classList.add(STICKY_CLASS);
+      logo.classList.add(STICKY);
       return;
     }
 
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Remove logo's stickyness when it touched the bottom of his container (banner).
     if (logoRect.bottom >= bannerRect.bottom) {
-      logo.classList.remove(STICKY_CLASS);
+      logo.classList.remove(STICKY);
       logo.style.bottom = BOTTOM_POS; // stay at the bottom of your container!
     }
   });
